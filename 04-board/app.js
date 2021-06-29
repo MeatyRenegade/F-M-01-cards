a3c6faeccc0826e4b97d6cae3c1a15d34fae7608
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-06-29 18:45:54 +0300

    мини игра добавлена

---
+++ 04-board/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board');
+const colors = ['#e73cd9', '#27c1e7', '#e47e1e', '#39ec16', 'rgb(236, 11, 86)'];
+const SQUARES_NUMBER = 400;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseleave', () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
